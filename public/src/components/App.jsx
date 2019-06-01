70a2dca6c40916df27707ba5de25f6cf869f4261
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-06-01 15:47:31 -0700

    Add functional webpack config and react file structure.

---
+++ public/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="dash">
+       <h1>Hello</h1>
+
+      </div>
+    )
+  }
+}
+
+export default App;
